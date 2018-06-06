8a3b0385c0f3205cfaa25e8e2ff8eeb3acb5ebae
wyunfei <583514368@qq.com>
2018-06-06 23:06:59 +0800

    sass模块化

--- src/layout/layout/layout.js
+++ src/layout/layout/layout.js
@@ -1,13 +1,14 @@
 /*Created by SmallFour on 2018/6/6*/
 import Nav from 'src/layout/nav/nav'
+import L from './layout.scss'
 export default class Layout extends React.Component{
     constructor(props) {
         super(props)
     }
     render() {
         return (
-            <div>
-                <Nav/>
+            <div className={L.layout}>
+                <h1><Nav/></h1>
             </div>
         )
     }
